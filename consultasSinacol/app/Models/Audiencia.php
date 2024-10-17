8d5d723589a6b0aa81985d5563f3339f29915b74
Sebastiansio <sebastiansiordia20@gmail.com>
2024-10-17 11:25:32 -0600

    facades db

--- consultasSinacol/app/Models/Audiencia.php
+++ consultasSinacol/app/Models/Audiencia.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\AppendPolicies;
