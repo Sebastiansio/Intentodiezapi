e51237d2b503140b5e0c3e5e50a9847144f2ec42
Sebastiansio <sebastiansiordia20@gmail.com>
2024-10-23 15:19:01 -0600

    s

--- consultasSinacol/app/Http/Controllers/AudienciasController.php
+++ consultasSinacol/app/Http/Controllers/AudienciasController.php
@@ -57,52 +57,54 @@ class AudienciasController extends Controller
         }
 
         public function getAudiencias(Request $request)
-        {
-            // Obtener la fecha del request o usar la fecha actual
-            $fecha = $request->input('fecha', date('Y-m-d'));
-            $fechaSiguiente = Carbon::parse($fecha)->addDay()->format('Y-m-d');
-        
-            $audiencias = Audiencia::conciliacionAudiencias($fecha, $fecha)->get();
-        
-            // Formatear los resultados para incluir los campos requeridos
-            $result = $audiencias->map(function ($item) {
-                // Decodificar 'solicitantes' y 'citados' si son cadenas JSON
-                $solicitantes = $item->solicitantes;
-                if (is_string($solicitantes)) {
-                    $solicitantes = json_decode($solicitantes, true) ?? [];
-                }
-        
-                $citados = $item->citados;
-                if (is_string($citados)) {
-                    $citados = json_decode($citados, true) ?? [];
-                }
-        
-                // Transformar 'solicitantes' en un array de objetos con clave 'Solicitante'
-                $solicitantesArray = array_map(function ($solicitante) {
-                    return ['Solicitante' => $solicitante];
-                }, $solicitantes);
-        
-                // Transformar 'citados' en un array de objetos con clave 'Citado'
-                $citadosArray = array_map(function ($citado) {
-                    return ['Citado' => $citado];
-                }, $citados);
-        
-                return [
-                    'Expediente' => $item->expediente,
-                    'Folio audiencia' => $item->audiencia,
-                    'Folio solicitud' => $item->folio_solicitud,
-                    'Fecha audiencia' => $item->fecha_evento,
-                    'Hora de inicio' => $item->hora_inicio,
-                    'Hora Fin' => $item->hora_termino,
-                    'Conciliador' => $item->conciliador,
-                    'Estatus' => $item->estatus,
-                    'Solicitantes' => $solicitantesArray,
-                    'Citados' => $citadosArray,
-                ];
-            });
-        
-            return response()->json($result);
+{
+    // Obtener la fecha del request o usar la fecha actual y luego obtener el día siguiente
+    $fecha = $request->input('fecha', date('Y-m-d'));
+    $fechaSiguiente = Carbon::parse($fecha)->addDay()->format('Y-m-d');
+
+    // Obtener las audiencias para el día siguiente
+    $audiencias = Audiencia::whereDate('fecha_evento', $fechaSiguiente)->get();
+
+    // Formatear los resultados para incluir los campos requeridos
+    $result = $audiencias->map(function ($item) {
+        // Decodificar 'solicitantes' y 'citados' si son cadenas JSON
+        $solicitantes = $item->solicitantes;
+        if (is_string($solicitantes)) {
+            $solicitantes = json_decode($solicitantes, true) ?? [];
         }
+
+        $citados = $item->citados;
+        if (is_string($citados)) {
+            $citados = json_decode($citados, true) ?? [];
+        }
+
+        // Transformar 'solicitantes' en un array de objetos con clave 'Solicitante'
+        $solicitantesArray = array_map(function ($solicitante) {
+            return ['Solicitante' => $solicitante];
+        }, $solicitantes);
+
+        // Transformar 'citados' en un array de objetos con clave 'Citado'
+        $citadosArray = array_map(function ($citado) {
+            return ['Citado' => $citado];
+        }, $citados);
+
+        return [
+            'Expediente' => $item->expediente,
+            'Folio audiencia' => $item->audiencia,
+            'Folio solicitud' => $item->folio_solicitud,
+            'Fecha audiencia' => $item->fecha_evento,
+            'Hora de inicio' => $item->hora_inicio,
+            'Hora Fin' => $item->hora_termino,
+            'Conciliador' => $item->conciliador,
+            'Estatus' => $item->estatus,
+            'Solicitantes' => $solicitantesArray,
+            'Citados' => $citadosArray,
+        ];
+    });
+
+    return response()->json($result);
+}
+
         
 
 }
