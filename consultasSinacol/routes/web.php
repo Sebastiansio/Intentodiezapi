c97f534bbbc2d73b47ec48784cd2341d5c4fc1e2
Sebastiansio <sebastiansiordia20@gmail.com>
2024-10-23 15:24:54 -0600

    dias

--- consultasSinacol/routes/web.php
+++ consultasSinacol/routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/audiencias', [AudienciasController::class, 'getAudiencias']);
+Route::get('/audiencias/dia-siguiente', [AudienciasController::class, 'getAudienciasDiaSiguiente']);
 Route::get('/mundo', [AudienciasController::class, 'getHolaMundo']);
 Route::get('/audiencias/por-dia', [AudienciasController::class, 'getAudienciasPorDia']);
